8a2599101c9b66750b85b727148095219bfa760c
asma <asma.alkathiiri@gmail.com>
2023-09-02 13:20:49 +0300

    Added App colors & spaces

---
+++ final_project/lib/ui/constants/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const orangeColor = Color(0xFFFE724C);
+const yellowColor = Color(0xFFFFC529);
+const darkColor = Color(0xFF1A1D26);
+const grayColor = Color(0xFF9A9FAE);
+const whiteColor = Colors.white;
+const blackColor = Colors.black;
