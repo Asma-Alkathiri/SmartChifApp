7017d1dbdb0b293aa011b9d20b2a59535c792052
mrzogy <magbolmrzogy@gmail.com>
2023-09-03 23:40:47 +0300

    add image

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:final_project/service/supabase_initializer.dart';
+import 'package:final_project/ui/componant/custom_google_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,12 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: GoogleButton());
   }
 }
