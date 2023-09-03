318d9c66763b0afe1f0d0405c4c2445f36ba8606
asma <asma.alkathiiri@gmail.com>
2023-09-03 19:43:32 +0300

    Added TextField & button

---
+++ final_project/lib/ui/componant/orange_button.dart
@@ -0,0 +1,37 @@
+import 'package:final_project/ui/constants/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+class OrangeButton extends StatelessWidget {
+  const OrangeButton(
+      {super.key,
+      this.width = 278,
+      this.height = 57,
+      this.onPressed,
+      required this.title});
+
+  final double width;
+  final double height;
+  final Function()? onPressed;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        elevation: 6,
+        shadowColor: orangeColor,
+        backgroundColor: orangeColor,
+        fixedSize: Size(width, height),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        title,
+        style: const TextStyle(
+            color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500),
+      ),
+    );
+  }
+}
