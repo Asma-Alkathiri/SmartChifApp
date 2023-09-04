d338a46884ddcd56bba432f62438112b59d70934
mrzogy <magbolmrzogy@gmail.com>
2023-09-04 14:08:39 +0300

    add image

---
+++ final_project/lib/ui/componant/coustom_textwithbutton.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TextWithTextButton extends StatelessWidget {
+  const TextWithTextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Already have an account?",
+          style: TextStyle(fontSize: 14),
+        ),
+        const SizedBox(
+          width: 1,
+        ),
+        InkWell(
+          onTap: () {},
+          child: const Text(
+            "Login",
+            style: TextStyle(color: Colors.orange),
+          ),
+        )
+      ],
+    );
+  }
+}
