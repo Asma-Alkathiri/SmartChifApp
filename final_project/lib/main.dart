456ae539e9f44682d95e4ddf63be5bb8ad3add41
mrzogy <magbolmrzogy@gmail.com>
2023-09-04 17:07:33 +0300

    Finish Reset Password Secreen

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:final_project/service/supabase_initializer.dart';
-import 'package:final_project/ui/componant/custom_google_button.dart';
-import 'package:final_project/ui/screens/phone_screens/OTP_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/sign_in_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/sign_up_screen.dart';
+import 'package:final_project/ui/screens/phone_screens/resetpassword.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: OTPScreen());
+    return const MaterialApp(home: ResetPassword());
   }
 }
