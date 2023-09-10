a71a538ea2f0f0fd477f4f41ef15a343f534b48b
Abdulrazaq Ali <121042333+Mrzogy@users.noreply.github.com>
2023-09-10 18:34:38 +0300

    Finish Tablet desaign

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -2,26 +2,13 @@ import 'dart:ui';
 
 import 'package:final_project/firebase_options.dart';
 import 'package:final_project/service/supabase_initializer.dart';
-
-import 'package:final_project/ui/componant/coustom_navigationbar.dart';
-
 import 'package:final_project/theme/dark_theme.dart';
 import 'package:final_project/theme/light_theme.dart';
-import 'package:final_project/ui/componant/home_container.dart';
-import 'package:final_project/ui/screens/phone_screens/Account_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/Registration_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/Suggestions_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/authentication_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/favourite_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/for_gpt.dart';
-import 'package:final_project/ui/screens/phone_screens/history_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/home_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/onboarding_screen.dart';
-import 'package:final_project/ui/screens/phone_screens/resetpassword.dart';
-import 'package:final_project/ui/screens/phone_screens/welcome_screen.dart';
+import 'package:final_project/ui/screens/tablet_screens/screens/tablet_Account_screen.dart';
+import 'package:final_project/ui/screens/tablet_screens/screens/tablet_authentication_screeen.dart';
+import 'package:final_project/ui/screens/tablet_screens/screens/tablet_home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -46,7 +33,7 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       darkTheme: DarkTheme,
-      home: const ForGpt(),
+      home: const TabletAccountScreen(),
     );
   }
 }
---
+++ final_project/lib/ui/screens/tablet_screens/screens/tablet_Account_screen.dart
@@ -0,0 +1,98 @@
+import 'package:final_project/ui/componant/custom_Account_Appbar.dart';
+import 'package:final_project/ui/componant/orange_button.dart';
+import 'package:final_project/ui/componant/text_field.dart';
+import 'package:final_project/ui/constants/custom_colors.dart';
+import 'package:final_project/ui/constants/custom_spacing.dart';
+import 'package:flutter/material.dart';
+
+class TabletAccountScreen extends StatelessWidget {
+  const TabletAccountScreen({super.key, this.name = "Name"});
+
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  height: 270,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                const AccountAppBar(
+                  widthappbar: 160,
+                  width: 200,
+                ),
+                Positioned(
+                  top: 100,
+                  right: 0,
+                  left: 0,
+                  child: Column(
+                    children: [
+                      const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://images.pexels.com/photos/18270600/pexels-photo-18270600/free-photo-of-indian-traditional-bride-in-nauvari-traditional-attire.jpeg"),
+                        radius: 55,
+                      ),
+                      kVSpace8,
+                      Text(
+                        name,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      kVSpace8,
+                      const Text(
+                        "Edit Profile",
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            color: grayColor),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            kVSpace16,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  const TextFieldWidget(
+                    textfontsize: 30,
+                    width: 900,
+                    hintText: "Full Name",
+                    label: "Full Name",
+                  ),
+                  kVSpace16,
+                  const TextFieldWidget(
+                    textfontsize: 30,
+                    width: 900,
+                    hintText: "E-mail",
+                    label: "E-mail",
+                  ),
+                  kVSpace16,
+                  const TextFieldWidget(
+                    textfontsize: 30,
+                    width: 900,
+                    hintText: "Phone Number",
+                    label: "Phone Number",
+                  ),
+                  kVSpace32,
+                  OrangeButton(
+                    width: 700,
+                    title: "SAVE",
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
